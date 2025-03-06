7de456ce9a8d42e3ca2a2a2eb5ec966f38197fc1
nhatnt.elsa <nhat.nguyen@elsanow.io>
2025-03-07 01:16:49 +0700

    layout home screen

--- hackathon/hackathon/HomeTab/HomeTab.swift
+++ hackathon/hackathon/HomeTab/HomeTab.swift
@@ -4,41 +4,72 @@ import SwiftUI
 struct HomeTab: View {
   @State private var selectedItems = [PhotosPickerItem]()
   @State private var selectedImages = [Image]()
+  @State private var selectedImage: Image? = nil // Ảnh đang chọn để hiển thị trong màn hình chi tiết
 
   var body: some View {
-    NavigationView {
-      VStack(spacing: 16) {
-        NavigationStack {
-          ScrollView {
-            LazyVStack {
+    NavigationStack {
+      ScrollView {
+        VStack(spacing: 16) {
+          VStack(alignment: .leading) {
+            Text("Recently")
+              .font(.headline)
+              .foregroundStyle(.black)
+
+            LazyVGrid(
+              columns: Array(repeating: GridItem(.flexible(), spacing: 16), count: 3),
+              spacing: 16
+            ) {
               ForEach(0..<selectedImages.count, id: \.self) { i in
-                selectedImages[i]
-                  .resizable()
-                  .scaledToFit()
-                  .frame(width: 300, height: 300)
+                Button(action: {
+                  selectedImage = selectedImages[i] // Cập nhật ảnh được chọn
+                }) {
+                  selectedImages[i]
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 120, height: 80)
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(8)
+                    .clipped()
+                }
               }
             }
           }
-          .toolbar {
-            // Button Row
-            NavigationLink(destination: PhotoDetailsView(selectedImage: nil)) {
-              Text("Dummy Image")
-                .frame(maxWidth: .infinity)
-                .padding()
-                .background(Color.blue)
-                .foregroundColor(.black)
-                .cornerRadius(8)
-            }
-            PhotosPicker("Select images", selection: $selectedItems, matching: .images)
-          }
-          .onChange(of: selectedItems) {
-            Task {
-              selectedImages.removeAll()
+          Spacer()
+          VStack(alignment: .leading) {
+            Text("Trending Now")
+              .font(.headline)
+              .foregroundStyle(.black)
 
-              for item in selectedItems {
-                if let image = try? await item.loadTransferable(type: Image.self) {
-                  selectedImages.append(image)
+            LazyVGrid(
+              columns: Array(repeating: GridItem(.flexible(), spacing: 16), count: 3),
+              spacing: 16
+            ) {
+              ForEach(0..<selectedImages.count, id: \.self) { i in
+                Button(action: {
+                  selectedImage = selectedImages[i] // Cập nhật ảnh được chọn
+                }) {
+                  selectedImages[i]
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 120, height: 80)
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(8)
+                    .clipped()
                 }
+              }
+            }
+          }
+        }
+        .padding(.horizontal)
+        .toolbar {
+          PhotosPicker("Select Images", selection: $selectedItems, matching: .images)
+        }
+        .onChange(of: selectedItems) { _ in
+          Task {
+            selectedImages.removeAll()
+            for item in selectedItems {
+              if let image = try? await item.loadTransferable(type: Image.self) {
+                selectedImages.append(image)
               }
             }
           }
@@ -50,7 +81,6 @@ struct HomeTab: View {
     .ignoresSafeArea()
   }
 }
-
 #Preview {
   HomeTab()
 }
